faa1ccdfa55def2041aebcb8d6acd40b9f7a3b01
Rida Serroukh <rida.serroukh@gmail.com>
2022-08-01 19:08:21 +0100

    Address fields and properties added.

--- PropertyRental/Address.cs
+++ PropertyRental/Address.cs
@@ -8,10 +8,34 @@ namespace PropertyRental
 {
     public class Address
     {
-        public int DoorNumber;
-        public string Street;
-        public string City;
-        public string PostCode;
+        private int _doorNumber;
+        private string _street;
+        private string _city;
+        private string _postCode;
+
+        public int DoorNumber
+        {
+            get { return _doorNumber; }
+            set { _doorNumber = value; }
+        }
+
+        public string Street
+        {
+            get { return _street; }
+            set { _street = value; }
+        }
+
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        public string PostCode
+        {
+            get { return _postCode; }
+            set { _postCode = value; }
+        }
 
     }
 }
