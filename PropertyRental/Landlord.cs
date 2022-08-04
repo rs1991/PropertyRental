c5c3c5f3ddce60fcb4f55c370950d677d8c7e1a7
Rida Serroukh <rida.serroukh@gmail.com>
2022-08-04 20:14:08 +0100

    Landlord class added and some more fields and properties added to different classes.

---
+++ PropertyRental/Landlord.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyRental
+{
+    public class Landlord
+    {
+        private Address _address;
+        private string _firstName;
+        private string _lastName;
+        private string _contactInformation;
+
+
+        public Address Address
+        { 
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string ContactInformation
+        {
+            get { return _contactInformation; }
+            set { _contactInformation = value; }
+        }
+    }
+}
