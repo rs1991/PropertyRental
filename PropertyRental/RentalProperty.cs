1a539a85c1756928f85f23d87b6a5f485b52dd04
Rida Serroukh <rida.serroukh@gmail.com>
2022-08-05 21:29:54 +0100

    Agency property added to RentalProperty class.

--- PropertyRental/RentalProperty.cs
+++ PropertyRental/RentalProperty.cs
@@ -11,13 +11,50 @@ namespace PropertyRental
         private double _rentalPrice;
         private Address _address;
         private DateOnly _availableOn;
-        //Number of rooms
-        //Type House or flat
-        //Description
-        //Gas or electric heating
-        //Council tax band 
-        //Agency 
+        private int _bedRooms;
+        private List<RentalProperty> _propertyType;
+        private string _description;
+        private List<RentalProperty> _heating;
+        private List<RentalProperty> _councilTaxBand;
+        private string _agency;
+
+
+
+
+
+     
+        public List<RentalProperty> Heating
+        {
+            get { return _heating; }
+            set { _heating = value; }
+        }
         
+        //Council tax band 
+        public List<RentalProperty> CouncilTaxBand
+        {
+            get { return _councilTaxBand; }
+            set { _councilTaxBand = value; }
+        }
+                
+        public string Agency
+        {
+            get { return _agency; }
+            set { _agency = value; }
+        }
+
+            
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public List<RentalProperty> PropertyType 
+        { 
+            get { return _propertyType; }
+            set { _propertyType = value; }
+        }
+
         public double RentalPrice
         {
             get { return _rentalPrice; }
@@ -34,6 +71,12 @@ namespace PropertyRental
         {
             get { return _availableOn; }
             set { _availableOn = value; }
+        }
+
+        public int BedRooms
+        {
+            get { return _bedRooms; }
+            set { _bedRooms = value; }
         }
 
     }
