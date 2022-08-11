740b94ce04c5a6b41f4f3d4efa8d5e0e5aa7df40
Rida Serroukh <rida.serroukh@gmail.com>
2022-08-11 23:10:12 +0100

    enum added for the EnergyType

---
+++ PropertyRental/Energy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyRental
+{
+    public class Energy
+    {
+
+        public enum EnergyType
+        {
+            Gas,
+            Electric,
+            Solar
+        }
+    }
+}
