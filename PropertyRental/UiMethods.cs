3f97b6d8bf6b18a6edd9e6614cde611c7a64904e
Rida Serroukh <rida.serroukh@gmail.com>
2022-08-29 00:16:40 +0100

    List of tenants added.

--- PropertyRental/UiMethods.cs
+++ PropertyRental/UiMethods.cs
@@ -13,56 +13,58 @@ namespace PropertyRental
             Console.WriteLine("Welcome to the app!");
         }
 
-        public static Tenant GenerateTenant()
-        {
-            Tenant tenantInfo = new Tenant();
-            
-            Console.WriteLine("What is your first name? ");
-            string tenantFirstName = Console.ReadLine();
-            tenantInfo.FirstName = tenantFirstName;
-            
-            Console.WriteLine("What is your last name? ");
-            string tenantLastName = Console.ReadLine();
-            tenantInfo.LastName = tenantLastName;
-
-            Console.WriteLine("What is your DOB? ");
-            DateTime DOB = Convert.ToDateTime(Console.ReadLine());
-            tenantInfo.BirthDate = DOB;
-            
-            Console.WriteLine("What is your Job title? ");
-            string jobTitle = Console.ReadLine();
-            tenantInfo.JobTitle = jobTitle;
-            
-            Console.WriteLine("What is your nationality? ");
-            string nationality = Console.ReadLine();
-            tenantInfo.Nationality = nationality;
-            
-            Console.WriteLine("What is your annual salary? ");
-            double salary = Console.Read();
-            tenantInfo.Salary = salary;
-            
-            Console.WriteLine("Number of children?");
-            int children = Console.Read();
-            tenantInfo.Children = children;
-            
-            bool pets = false;
-            Console.WriteLine("Any pets?");
-            string petResponse = Console.ReadLine();
-            if (petResponse == "y")
-            {
-                pets = true;
-            }
+        //
+        //public static Tenant GenerateTenant()
+        //{
+        //  Tenant tenantInfo = new Tenant();
+        //    
+        //    Console.WriteLine("What is your first name? ");
+        //    string tenantFirstName = Console.ReadLine();
+        //    tenantInfo.FirstName = tenantFirstName;
+        //    
+        //    Console.WriteLine("What is your last name? ");
+        //    string tenantLastName = Console.ReadLine();
+        //    tenantInfo.LastName = tenantLastName;
+        //
+        //    
+        //    Console.WriteLine("What is your DOB? ");
+        //    string DOB = Console.ReadLine();
+        //    tenantInfo.BirthDate = DOB;
+        //    
+        //    Console.WriteLine("What is your Job title? ");
+        //    string jobTitle = Console.ReadLine();
+        //    tenantInfo.JobTitle = jobTitle;
+        //    
+        //    Console.WriteLine("What is your nationality? ");
+        //   string nationality = Console.ReadLine();
+        //    tenantInfo.Nationality = nationality;
+        //    
+        //    Console.WriteLine("What is your annual salary? ");
+        //    double salary = Console.Read();
+        //    tenantInfo.Salary = salary;
+        //    
+        //    Console.WriteLine("Number of children?");
+        //    int children = Console.Read();
+        //    tenantInfo.Children = children;
+        //    
+        //    bool pets = false;
+        //    Console.WriteLine("Any pets?");
+       //     string petResponse = Console.ReadLine();
+         //   if (petResponse == "y")
+           // {
+             //   pets = true;
+           // }
           //  pets = Convert.ToBoolean(Console.ReadLine());
-            tenantInfo.Pets = pets;
+            //tenantInfo.Pets = pets;
             
-            bool smoker;
-            Console.WriteLine("Are you a smoker?");
-            smoker = Convert.ToBoolean(Console.ReadLine());
-            tenantInfo.Smoker = smoker;
+            //bool smoker;
+            //Console.WriteLine("Are you a smoker?");
+            //smoker = Convert.ToBoolean(Console.ReadLine());
+           // tenantInfo.Smoker = smoker;
             
-            Console.WriteLine("How long are you looking to rent for?");
-            int rentalDuration = Console.Read();
-            tenantInfo.RentalTerm = rentalDuration;
+            //Console.WriteLine("How long are you looking to rent for?");
+            //int rentalDuration = Console.Read();
+            //tenantInfo.RentalTerm = rentalDuration;
 
             //Address address; 
             //Console.WriteLine("What is your current address? ");
@@ -71,9 +73,9 @@ namespace PropertyRental
             
             //private Address _address;
             //private List<Tenant> _roomMates;
-            return tenantInfo;
+//            return tenantInfo;
         }
         
 
     }
-}
+
