c5fca7125e48b483f6e1b38ae40a3fad4287c6cf
Rida Serroukh <rida.serroukh@gmail.com>
2022-08-30 21:21:01 +0100

    CouncilTax enum added

---
+++ PropertyRental/CouncilTaxBand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyRental
+{
+    public class CouncilTaxBand
+    {
+        public enum CouncilTax
+        {
+            Band_A,
+            Band_B,
+            Band_C,
+            Band_D
+        }
+    }
+}
