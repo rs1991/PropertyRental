d1f2a4d71e8f788767e878d21f5fcc19efb69d6b
Rida Serroukh <rida.serroukh@gmail.com>
2022-10-03 21:34:29 +0100

    Gender enum class added.

---
+++ PropertyRental/Gender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PropertyRental
+
+{
+    [Flags]
+    public enum Gender
+    {
+        Male = 0,
+        Female = 1,
+        Other = 2
+    }
+ }
