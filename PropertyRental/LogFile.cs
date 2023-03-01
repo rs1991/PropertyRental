268bba80817d477e5d217d26abf971f94e656c12
Rida Serroukh <rida.serroukh@gmail.com>
2023-03-01 06:55:13 +0000

    App.config file deleted.

--- PropertyRental/LogFile.cs
+++ PropertyRental/LogFile.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Protocols;
+
 
 namespace PropertyRental
 {
     public static class LogFile
     {
+        public static void WriteLog()
+        {
+           
+
+        }
 
     }
 }
