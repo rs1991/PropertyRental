11465a0740553a2ad30c62d5dcc62c2e0495af64
Rida Serroukh <rida.serroukh@gmail.com>
2023-07-20 17:55:09 +0100

    Added a fix to the ContactInformation class #10
    Fix enables serialization of email addresses stored as MailAddress fields using XmlSerializer.

--- PropertyRental/ContactInformation.cs
+++ PropertyRental/ContactInformation.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Mail;
+using System.Xml.Serialization;
 
 namespace PropertyRental
 {
@@ -13,11 +14,23 @@ namespace PropertyRental
             set { _phoneNumber = value; }
         }
 
+        [XmlIgnore] /* The [XmlIgnore] attribute is applied to the Email property.
+                     * This attribute tells the XmlSerializer to exclude the Email property from direct XML serialization.
+                     * Instead, the email address will be serialized using a custom approach.  
+                     */
         public MailAddress Email
         {
             get { return _email; }
             set { _email = value; }
         }
+
+        [XmlElement("SerializedEmail")]
+        public string SerializedEmail
+        {
+            get { return _email?.Address; }
+            set { _email = value != null ? new MailAddress(value) : null; }
+        }
+
 
         public ContactInformation()
         {
